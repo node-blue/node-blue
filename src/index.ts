bb90d8c002f1eb78b231749263f8932401a9b4a8
Niels Bik <nielsrowinbik@gmail.com>
2020-05-29 21:51:33 +0200

    Add command line option to set the API path

--- src/index.ts
+++ src/index.ts
@@ -20,9 +20,10 @@ program
     .command("start [nodes]")
     .description(`Start ${name}`)
     .option("-h, --host <host>", "your Home Assistant instance's host")
-    .option("-t, --token <token>", "a long-lived access token")
+    .option("-P, --path <path>", "path to the Websocket API", "/api/websocket")
     .option("-p, --port <port>", "your Home Assistant instance's port", "8123")
     .option("-s, --secure", "use a secure connection")
+    .option("-t, --token <token>", "a long-lived access token")
     .action(init);
 
 program.parse(process.argv);
